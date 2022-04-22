e0964edb689f3f4247dfd9f45858fe6fa519216f
Thane Chaves <tmoonder@gmail.com>
2022-04-22 16:48:21 -0400

    Basic functionality for stage one complete.

--- mastermind.rb
+++ mastermind.rb
@@ -1,10 +1,10 @@
 class Game
-  attr_reader :secret_hash, :guess_hash, :hint_hash
+  attr_reader :secret_array, :guess_array, :hint_array, :game_over
 
-  def initialize(secret_hash)
-    @secret_hash = secret_hash
+  def initialize(secret_array)
+    @secret_array = secret_array
     @game_over = false
-    @hint_hash = { one: '-', two: '-', three: '-', four: '-' }
+    @hint_array = ['-', '-', '-', '-']
   end
 
   def turn
@@ -18,25 +18,36 @@ class Game
     puts 'Please input your fourth choice.'
     @choice_four = gets.chomp
     # sanitize the input later
-    @guess_hash = { one: @choice_one, two: @choice_two, three: @choice_three, four: @choice_four }
+    @guess_array = [ @choice_one, @choice_two, @choice_three, @choice_four]
   end
 
-  def win_check(secret_hash, guess_hash)
-    if secret_hash == guess_hash
+  def win_check(secret_array, guess_array)
+    if secret_array == guess_array
       puts 'you guessed it!'
       @game_over = true
     end
   end
 
-  def hint_generator(secret_hash, guess_hash, hint_hash)
-    # create for loop to test each item in the array
-    # every time they exist push W
-    # create a second loop to check exacts
-    # every time those exist, push BL
+  def hint_generator(secret_array, guess_array, hint_array)
+    i = 0
+    for i in i..3
+      if guess_array[i] == secret_array[i]
+        hint_array[i] = 'BL'
+      elsif secret_array.include? guess_array[i]
+        hint_array[i] = 'W'
+      else
+      end
+    end
   end
 end
 
-game = Game.new({ one: 'R', two: 'R', three: 'R', four: 'R'})
-game.turn
-game.win_check(game.secret_hash, game.guess_hash)
-game.hint_generator(game.secret_hash, game.guess_hash, game.hint_hash)
+game = Game.new(['R', 'R', 'R', 'R'])
+
+while game.game_over == false
+  game.turn
+  game.win_check(game.secret_array, game.guess_array)
+  break if game.game_over == true
+  game.hint_generator(game.secret_array, game.guess_array, game.hint_array)
+  puts "Not quite. here's how close you came:"
+  puts game.hint_array
+end
