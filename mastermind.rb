2dce4428230b2e7b2d491960691dc56e227e7c53
Thane Chaves <tmoonder@gmail.com>
2022-04-23 08:13:08 -0400

    Adding in more basics to the secret maker game.

--- mastermind.rb
+++ mastermind.rb
@@ -36,7 +36,7 @@ class GuesserGame
 
   def win_check(secret_array, guess_array)
     if secret_array == guess_array
-      puts 'you guessed it!'
+      puts 'You guessed it!'
       @game_over = true
     end
   end
@@ -45,18 +45,30 @@ class GuesserGame
     i = 0
     for i in i..3
       if guess_array[i] == secret_array[i]
-        hint_array[i] = 'BL'
+        hint_array[i] = 'X'
       elsif secret_array.include? guess_array[i]
-        hint_array[i] = 'W'
+        hint_array[i] = 'O'
       else
       end
     end
   end
 end
 
-class SecretGame
+class SecretGame < GuesserGame
+  attr_reader :secret_array, :hint_array
+
   def initialize
-    # asks player to input code, store this as secret array
+    puts 'You have four possible colors: R, Y, G, and B.'
+    puts 'Please input your first choice.'
+    @choice_one = gets.chomp
+    puts 'Please input your second choice.'
+    @choice_two = gets.chomp
+    puts 'Please input your third choice.'
+    @choice_three = gets.chomp
+    puts 'Please input your fourth choice.'
+    @choice_four = gets.chomp
+    # sanitize the input later
+    @secret_array = [ @choice_one, @choice_two, @choice_three, @choice_four]
   end
 
   def computer_turn
@@ -68,29 +80,35 @@ class SecretGame
 
   def player_turn
     puts 'How close did I get?'
+    puts 'An X means an exact guess, and an O means right color, wrong spot. A - means neither.'
     puts 'First choice:'
+    @hint_one = gets.chomp
     puts 'Second choice:'
+    @hint_two = gets.chomp
     puts 'Third choice:'
+    @hint_three = gets.chomp
     puts 'Fourth choice:'
-    # and then collect all these as an array.
+    @hint_four = gets.chomp
+    @hint_array = [ @hint_one, @hint_two, @hint_three, @hint_four ]
   end
 end
 
-puts "Hello! Type G to be the guesser or S to be the secret code maker."
+puts 'Hello! Type G to be the guesser or S to be the secret code maker.'
 choice = gets.chomp
 if choice == 'G'
-  game = GuesserGame.new(['R', 'R', 'R', 'R'])
+  game = GuesserGame.new
 
   while game.game_over == false
     game.turn
     game.win_check(game.secret_array, game.guess_array)
     break if game.game_over == true
     game.hint_generator(game.secret_array, game.guess_array, game.hint_array)
-    puts "Not quite. here's how close you came:"
+    puts "Not quite. An X means an exact guess, and an O means right color, wrong spot."
+    puts "Here's how close you came:"
     puts game.hint_array
   end
 elsif choice == 'S'
-  # run the secret maker game
+  game = SecretGame.new
 else
   puts "Invalid input!"
 end
