3bb4a88f8adae426051f15f1066fb93a39ecf0ba
Thane Chaves <tmoonder@gmail.com>
2022-04-23 07:48:54 -0400

    Initial setup code for version two.

--- mastermind.rb
+++ mastermind.rb
@@ -1,7 +1,7 @@
-class Game
+class GuesserGame
   attr_reader :secret_array, :guess_array, :hint_array, :game_over
 
-  def initialize(secret_array)
+  def initialize
     @secret_array = []
     @game_over = false
     @hint_array = ['-', '-', '-', '-']
@@ -54,13 +54,43 @@ class Game
   end
 end
 
-game = Game.new(['R', 'R', 'R', 'R'])
+class SecretGame
+  def initialize
+    # asks player to input code, store this as secret array
+  end
 
-while game.game_over == false
-  game.turn
-  game.win_check(game.secret_array, game.guess_array)
-  break if game.game_over == true
-  game.hint_generator(game.secret_array, game.guess_array, game.hint_array)
-  puts "Not quite. here's how close you came:"
-  puts game.hint_array
+  def computer_turn
+    # if first turn, generate random code to guess at
+    # if not: keep anything BL
+    # anything with - use W if possible
+    # work out more sophisticated logic later that's a lot
+  end
+
+  def player_turn
+    puts 'How close did I get?'
+    puts 'First choice:'
+    puts 'Second choice:'
+    puts 'Third choice:'
+    puts 'Fourth choice:'
+    # and then collect all these as an array.
+  end
+end
+
+puts "Hello! Type G to be the guesser or S to be the secret code maker."
+choice = gets.chomp
+if choice == 'G'
+  game = GuesserGame.new(['R', 'R', 'R', 'R'])
+
+  while game.game_over == false
+    game.turn
+    game.win_check(game.secret_array, game.guess_array)
+    break if game.game_over == true
+    game.hint_generator(game.secret_array, game.guess_array, game.hint_array)
+    puts "Not quite. here's how close you came:"
+    puts game.hint_array
+  end
+elsif choice == 'S'
+  # run the secret maker game
+else
+  puts "Invalid input!"
 end
